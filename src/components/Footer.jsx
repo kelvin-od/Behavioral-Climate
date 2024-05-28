7ee45958e84b1437e9ec96fc37494d8b384c2991
MindCraftGT <dnyaks32@gmail.com>
2024-05-28 16:56:39 +0300

    fixed broken link in the footer.jsx file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -10,26 +11,26 @@ const Footer = () => {
                 <div className='flex-col items-center justify-between'>
                     <p className='text-white text-2xl'>About Green Tech</p>
                     <div className='flex flex-col text-xl py-4 pl-2 leading-loose'>
-                        <a className='text-gray-300 hover:text-gray-100'  href="">Committed to change</a>
-                        <a className='text-gray-300 hover:text-gray-100' href="">Committed to Diversity</a>
-                        <a className='text-gray-300 hover:text-gray-100' href="">Careers</a>
+                        <Link className='text-gray-300 hover:text-gray-100'  to="">Committed to change</Link>
+                        <Link className='text-gray-300 hover:text-gray-100' to="">Committed to Diversity</Link>
+                        <Link className='text-gray-300 hover:text-gray-100' to="">Careers</Link>
                     </div>
                 </div>
 
                 <div className='flex-col items-center justify-between'>
                     <p className='text-white text-2xl'>News and Resources</p>
                     <div className='flex flex-col text-xl py-4 text-gray-300 pl-2 leading-loose'>
-                        <a className='text-gray-300 hover:text-gray-100' href="">News & Update</a>
-                        <a className='text-gray-300 hover:text-gray-100' href="">Reports & Publication</a>
-                        <a className='text-gray-300 hover:text-gray-100' href=""> For the media</a>
+                        <Link className='text-gray-300 hover:text-gray-100' to="">News & Update</Link>
+                        <Link className='text-gray-300 hover:text-gray-100' to="">Reports & Publication</Link>
+                        <Link className='text-gray-300 hover:text-gray-100' to=""> For the media</Link>
                     </div>
                 </div>
 
                 <div className='flex-col items-center justify-between'>
                     <p className='text-white text-2xl'>Become the change</p>
                     <div className='flex flex-col text-xl py-4 text-gray-300 pl-2 leading-loose'>
-                        <a className='text-gray-300 hover:text-gray-100' href="">Support</a>
-                        <a className='text-gray-300 hover:text-gray-100' href="/components/contact">Contact</a>
+                        <Link className='text-gray-300 hover:text-gray-100' to="">Support</Link>
+                        <Link className='text-gray-300 hover:text-gray-100' to="/contactus">Contact</Link>
                     </div>
                 </div>
 

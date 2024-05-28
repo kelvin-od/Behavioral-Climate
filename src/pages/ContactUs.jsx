24d9243550662127a6e0b03fb3d4b1353488c509
MindCraftGT <dnyaks32@gmail.com>
2024-05-28 16:13:39 +0300

    add some changes as illustrated

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -5,14 +5,14 @@ import Bottomfooter from '../components/Bottomfooter';
 const ContactUs = () => {
   return (
     <>
-      <div>
+      <div className='antialiased bg-gray-100 flex flex-row gap-11 w-full min-h-screen items-center justify-center mt-4'>
         <div>
           <h2> Contact Us </h2>
           <p>Location, <span>Nairobi, Kenya</span></p>
           <p>+254 726422076</p>
           <p>Fax: XXX </p>
         </div>
-        <div>
+        <div className='flex flex-col w-50% border-spacing-y-6'>
           <fieldset>
             <label htmlFor="">Subscribe to Our Periodic News Letter</label>
             <form action="">
@@ -26,6 +26,7 @@ const ContactUs = () => {
               <input type="email" />
               <label htmlFor="">Let's Know Something About Your Passion to the Environment</label>
               <textarea name="" id="" cols="30" rows="10" placeholder='Message' typeof='text'></textarea>
+              <button>Submit</button>
             </form>
           </fieldset>
         </div>

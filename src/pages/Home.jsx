71bedc4236945358cdafbe0d9f57d4821bb51350
MindCraftGT <dnyaks32@gmail.com>
2024-05-28 17:19:36 +0300

    added main_three.jsx to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,21 @@ import Main_one from '../components/Main_one';
 import Main_two from '../components/Main_two';
 import Footer from '../components/Footer';
 import Bottomfooter from '../components/Bottomfooter';
+import Main_zero from '../components/Main_zero';
+import Main_three from '../components/Main_three'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Main />
+      <Main_zero />
       <Main_one />
       <Main_two />
+      <Main_three />
       <Footer />
       <Bottomfooter />
+
     </>
   )
 }

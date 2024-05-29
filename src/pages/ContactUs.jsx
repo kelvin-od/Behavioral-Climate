dd93119c5b3ba22a74f065a0d01558ab8b745349
MindCraftGT <dnyaks32@gmail.com>
2024-05-29 13:02:17 +0300

    added some styling on contactus form

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -5,18 +5,18 @@ import Bottomfooter from '../components/Bottomfooter';
 const ContactUs = () => {
   return (
     <>
-      <div className='antialiased bg-gray-100 flex flex-row gap-11 w-full min-h-screen items-center justify-center mt-4'>
-        <div>
+      <div className=' bg-red-500 flex flex-row gap-10 items-center h-screen w-full p-4'>
+        <div className='w-1/4 '>
           <h2> Contact Us </h2>
           <p>Location, <span>Nairobi, Kenya</span></p>
           <p>+254 726422076</p>
           <p>Fax: XXX </p>
         </div>
-        <div className='flex flex-col w-50% border-spacing-y-6'>
-          <fieldset>
+        <div className='w-'>
+          <fieldset className='flex flex-col items-center'>
             <label htmlFor="">Subscribe to Our Periodic News Letter</label>
-            <form action="">
-              <div>
+            <form action="" className='flex flex-col'>
+              <div className='flex col'>
                 <label htmlFor="">First Name</label>
                 <input type="text" />
                 <label htmlFor="">Last Name</label>
@@ -25,7 +25,7 @@ const ContactUs = () => {
               <label htmlFor="">Email</label>
               <input type="email" />
               <label htmlFor="">Let's Know Something About Your Passion to the Environment</label>
-              <textarea name="" id="" cols="30" rows="10" placeholder='Message' typeof='text'></textarea>
+              <textarea name="" id="" cols="10" rows="10" placeholder='Message' typeof='text'></textarea>
               <button>Submit</button>
             </form>
           </fieldset>

19aa3812f46a58cc31a7d0afa6f1e93ce8775ec6
MindCraftGT <dnyaks32@gmail.com>
2024-06-01 11:36:07 +0300

    minor deletion

--- src/pages/Becomemember.jsx
+++ src/pages/Becomemember.jsx
@@ -69,24 +69,6 @@ const Becomemember = () => {
             </div>
           </div>
         </div>
-        {/* <h2 className='flex flex-col items-center justify-center mt-10 mb-5'>Fill the Form and Submit to Become a Member</h2>
-          <form action="" className='flex flex-col items-center justify-center'>
-            <div className='flex flex-col items-center justify-center'>
-              <label htmlFor="">First Name</label>
-              <input type="text" />
-              <label htmlFor=""> Last Name</label>
-              <input type="text" />
-              <label htmlFor="">Username</label>
-              <input type="text" />
-              <label htmlFor=""> Email</label>
-              <input type="text" />
-              <label htmlFor="">Password</label>
-              <input type="text" />
-              <label htmlFor="">Conform Password</label>
-              <input type="text" />
-            </div>
-            <button type="submit">Submit</button>
-          </form> */}
       </div>
       
       <Footer />
